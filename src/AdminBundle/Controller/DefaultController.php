b1ca50103bc37d88769c99eefaf0a2445141b3ed
Clément Gaudron <clement.gaudron@hotmail.com>
2016-12-24 15:39:53 +0100

    Ajout assetic, bootstrap en global et lib utiles pour adminBundle avec son layout perso

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/admin")
+ */
 class DefaultController extends Controller
 {
     /**
