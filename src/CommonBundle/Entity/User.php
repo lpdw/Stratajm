a858b126722b5e61befaae7ee566bb12e87ebec4
Clément Gaudron <clement.gaudron@hotmail.com>
2016-12-24 14:52:20 +0100

    Ajout entity member pour faire la dif entre les utilisateurs du sites et les membres de l'association. ( différentes infos )

--- src/CommonBundle/Entity/User.php
+++ src/CommonBundle/Entity/User.php
@@ -5,7 +5,7 @@ namespace CommonBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * User
+ * User = utilisateurs du site.
  *
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="CommonBundle\Repository\UserRepository")
@@ -22,34 +22,6 @@ class User
     private $id;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="firstName", type="string", length=255)
-     */
-    private $firstName;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="lastName", type="string", length=255)
-     */
-    private $lastName;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255, nullable=true, unique=true)
-     */
-    private $email;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="telNum", type="integer", nullable=true, unique=true)
-     */
-    private $telNum;
-
-    /**
      * Many Users have Many Roles.
      * @ORM\ManyToMany(targetEntity="Role")
      * @ORM\JoinTable(name="user_role",
@@ -58,9 +30,6 @@ class User
      *      )
      */
     private $role;
-
-    
-
     /**
      * Get id
      *
@@ -69,102 +38,6 @@ class User
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * Set firstName
-     *
-     * @param string $firstName
-     *
-     * @return User
-     */
-    public function setFirstName($firstName)
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-
-    /**
-     * Get firstName
-     *
-     * @return string
-     */
-    public function getFirstName()
-    {
-        return $this->firstName;
-    }
-
-    /**
-     * Set lastName
-     *
-     * @param string $lastName
-     *
-     * @return User
-     */
-    public function setLastName($lastName)
-    {
-        $this->lastName = $lastName;
-
-        return $this;
-    }
-
-    /**
-     * Get lastName
-     *
-     * @return string
-     */
-    public function getLastName()
-    {
-        return $this->lastName;
-    }
-
-    /**
-     * Set email
-     *
-     * @param string $email
-     *
-     * @return User
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    /**
-     * Get email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Set telNum
-     *
-     * @param integer $telNum
-     *
-     * @return User
-     */
-    public function setTelNum($telNum)
-    {
-        $this->telNum = $telNum;
-
-        return $this;
-    }
-
-    /**
-     * Get telNum
-     *
-     * @return int
-     */
-    public function getTelNum()
-    {
-        return $this->telNum;
     }
     /**
      * Constructor
@@ -208,3 +81,4 @@ class User
         return $this->role;
     }
 }
+
