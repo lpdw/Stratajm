2d949f88e8c85ba5c61f6428c268ddfd0945e648
Vaultz <paulvialart@hotmail.fr>
2017-01-02 14:07:47 +0100

    Création de la page publique d'affichage des jeux

--- src/CommonBundle/Controller/DefaultController.php
+++ src/CommonBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace CommonBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -14,4 +15,20 @@ class DefaultController extends Controller
     {
         return $this->render('CommonBundle:Default:index.html.twig');
     }
+
+    /**
+     * @Route("/games", name="display_games")
+     */
+    public function displayGamesAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $games = $em->getRepository('CommonBundle:Game')->findAll();
+
+        return $this->render('CommonBundle:Default:displayGames.html.twig', array(
+            'games' => $games,
+        ));
+    }
+
+
 }
