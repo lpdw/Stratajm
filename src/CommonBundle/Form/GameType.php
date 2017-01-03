949906eb1011e632d709de731d72833f903dd23c
Vaultz <pvialart@MC-G-LP-C120-9.local>
2017-01-03 12:30:02 +0100

    Modif du formulaire de création de jeu

--- src/CommonBundle/Form/GameType.php
+++ src/CommonBundle/Form/GameType.php
@@ -5,6 +5,8 @@ namespace CommonBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 
 class GameType extends AbstractType
 {
@@ -13,9 +15,12 @@ class GameType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name')->add('duration')->add('age')->add('rules')->add('releaseDate')->add('themes')->add('types')->add('publisher')        ;
+        $builder->add('name')->add('duration')->add('age')->add('rules')->add('releaseDate')->add('themes')->add('types')->add('publisher', EntityType::class, array(
+          'class' => 'CommonBundle:Publisher',
+          'choice_label' => 'name'
+        ));
     }
-    
+
     /**
      * {@inheritdoc}
      */
