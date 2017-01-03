20d1e062ea3ec6516df07ac910209d3e9a066f9d
Margaux Smits <smits.margaux@gmail.com>
2017-01-03 11:01:39 +0100

    Adding FOSJSRouting bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new AdminBundle\AdminBundle(),
             new CommonBundle\CommonBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
