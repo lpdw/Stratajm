bc573e86be91a6d2bc702a341a7b21522de5057a
Margaux Smits <smits.margaux@gmail.com>
2017-01-03 11:02:36 +0100

    Autocompletion on search game by name input

--- src/CommonBundle/Controller/DefaultController.php
+++ src/CommonBundle/Controller/DefaultController.php
@@ -5,6 +5,12 @@ namespace CommonBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use CommonBundle\Form\GameSearchType;
+use CommonBundle\Form\GameSortType;
+
+use CommonBundle\Repository;
+use CommonBundle\Entity\Game;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -17,17 +23,42 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/games", name="display_games")
+     * @Route("/games", name="display_games",options={"expose"=true})
      */
     public function displayGamesAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $games = $em->getRepository('CommonBundle:Game')->findAll();
+        $gameSortForm = $this->createForm(GameSortType::class);
+        $gameSortForm->handleRequest($request);
 
-        return $this->render('CommonBundle:Default:displayGames.html.twig', array(
-            'games' => $games,
-        ));
+        $gameSearchForm = $this->createForm(GameSearchType::class);
+        $gameSearchForm->handleRequest($request);
+
+        $searchValue=$request->request->get('input');
+
+        //Gestion de l'autocompletion du champs de recherche par
+        if($searchValue!=null){
+          $gamesFound = $em->getRepository('CommonBundle:Game')->searchGame($searchValue);
+          return new JsonResponse(array('games'=>json_encode($gamesFound)));
+
+        }
+
+        if ($gameSearchForm->isSubmitted() && $gameSearchForm->isValid()) {
+          $name=$gameSearchForm['searchGame']->getData();
+          $gameFound = $em->getRepository('CommonBundle:Game')->searchGameByName($name);
+          return $this->render('CommonBundle:Default:displayGames.html.twig', array(
+              'games' => $gameFound,"searchForm"=>$gameSearchForm->createView(),"sortForm"=>$gameSortForm->createView()
+          ));
+        }
+
+          // Par défaut on affiche tous les jeux
+          $games = $em->getRepository('CommonBundle:Game')->findAll();
+
+          return $this->render('CommonBundle:Default:displayGames.html.twig', array(
+              'games' => $games,"searchForm"=>$gameSearchForm->createView(),"sortForm"=>$gameSortForm->createView()
+          ));
+
     }
 
 
--- src/CommonBundle/Repository/GameRepository.php
+++ src/CommonBundle/Repository/GameRepository.php
@@ -10,4 +10,23 @@ namespace CommonBundle\Repository;
  */
 class GameRepository extends \Doctrine\ORM\EntityRepository
 {
+
+  public function searchGame($searchValue){
+    return $this
+    ->createQueryBuilder('g')
+    ->select(array('g'))
+    ->where("g.name LIKE :searchValue")
+    ->setParameter('searchValue',"%".$searchValue."%")
+    ->getQuery()
+    ->getArrayResult();  }
+
+    public function searchGameByName($name){
+      return $this
+      ->createQueryBuilder('g')
+      ->where("g.name LIKE :name")
+      ->setParameter('name',"%".$name."%")
+      ->getQuery()
+      ->getSingleResult();  }
+
+
 }
