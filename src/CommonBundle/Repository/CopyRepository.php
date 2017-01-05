dfd2a1f7b7fd09b34c80a565f10f64b53cc7a5c2
Vaultz <paulvialart@hotmail.fr>
2017-01-05 14:27:14 +0100

    Fix d'un bug lors du comptage de nombre d'exemplaires d'un jeu

--- src/CommonBundle/Repository/CopyRepository.php
+++ src/CommonBundle/Repository/CopyRepository.php
@@ -14,9 +14,9 @@ class CopyRepository extends \Doctrine\ORM\EntityRepository
   public function countCopiesByGame($gameid) {
     return $this
       ->createQueryBuilder('c')
-      ->select('COUNT(c)')
-      ->where('c.game = :gameid')
-      ->setParameter('gameid', '%'.$gameid.'%')
+      ->select('COUNT(c.id)')
+      ->leftJoin('c.game', 'g', 'WITH', 'c.game = (:game_id)')
+      ->setParameter('game_id', '%'.$gameid.'%')
       ->getQuery()
       ->getSingleScalarResult();
   }
