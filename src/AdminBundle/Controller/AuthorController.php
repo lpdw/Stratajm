6d56e8d145d1205155a11759d1025cb5a3c83d84
Margaux SMITS <smits.margaux@gmail.com>
2017-01-13 11:49:26 +0100

    new entity author

---
+++ src/AdminBundle/Controller/AuthorController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace AdminBundle\Controller;
+
+use CommonBundle\Entity\Author;
+use AdminBundle\Services\CopyGeneratorService;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * Author controller.
+ *
+ * @Route("admin/author")
+ */
+class AuthorController extends Controller
+{
+    /**
+     * Lists all author entities.
+     *
+     * @Route("/", name="admin_author_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $authors = $em->getRepository('CommonBundle:Author')->findAll();
+
+        return $this->render('author/index.html.twig', array(
+            'authors' => $authors,
+        ));
+    }
+
+    /**
+     * Creates a new author entity.
+     *
+     * @Route("/new", name="admin_author_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $author = new Author();
+        $form = $this->createForm('CommonBundle\Form\AuthorType', $author);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush($author);
+
+            return $this->redirectToRoute('admin_author_show', array('id' => $author->getId()));
+        }
+
+        return $this->render('author/new.html.twig', array(
+            'author' => $author,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a author entity.
+     *
+     * @Route("/{id}", name="admin_author_show")
+     * @Method("GET")
+     */
+    public function showAction(Author $author)
+    {
+        $deleteForm = $this->createDeleteForm($author);
+
+        return $this->render('author/show.html.twig', array(
+            'author' => $author,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing author entity.
+     *
+     * @Route("/{id}/edit", name="admin_author_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Author $author)
+    {
+        $deleteForm = $this->createDeleteForm($author);
+        $editForm = $this->createForm('CommonBundle\Form\AuthorType', $author);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_author_edit', array('id' => $author->getId()));
+        }
+
+        return $this->render('author/edit.html.twig', array(
+            'author' => $author,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a author entity.
+     *
+     * @Route("/{id}", name="admin_author_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Author $author)
+    {
+        $form = $this->createDeleteForm($author);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($author);
+            $em->flush($author);
+        }
+
+        return $this->redirectToRoute('admin_author_index');
+    }
+
+    /**
+     * Creates a form to delete a author entity.
+     *
+     * @param Author $author The author entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Author $author)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_author_delete', array('id' => $author->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
